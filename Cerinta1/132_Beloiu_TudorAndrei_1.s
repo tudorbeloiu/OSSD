c374e368180d7ed626e2fcdb4f8630d4135e0e6a
tudorbeloiu <beloiutudorandrei@gmail.com>
2024-12-05 22:50:42 +0200

    Defragmentation but with some bugs

--- Cerinta1/132_Beloiu_TudorAndrei_1.s
+++ Cerinta1/132_Beloiu_TudorAndrei_1.s
@@ -8,12 +8,22 @@ numberOfBlocks: .space 4
 indLinie: .space 4
 indJST: .space 4
 indJDR: .space 4
+tempmemorie: .space 1048576
+distanta: .space 4
+elementinit: .space 4
+saveEAX: .space 4
+saveEBX: .space 4
+saveECX: .space 4
+saveEDX: .space 4
+linia: .space 4
+coloana: .space 4
 numberInput: .asciz "%d"
 op: .asciz "%d"
 getOutput: .asciz "((%d, %d), (%d, %d))\n"
 outputZero: .asciz "((0,0), (0,0))\n"
 intervalOutput: .asciz "%d: ((%d, %d), (%d, %d))\n"
 otpget: .asciz "((%d, %d), (%d, %d))\n"
+
 .text
 
 addfunction:
@@ -422,6 +432,247 @@ popl %ebp
 ret
 
 
+
+
+defragmentationfunction:
+pushl %ebp
+movl %esp,%ebp
+
+movl 32(%ebp),%edi
+
+movl $0,%esi
+movl $0,%ecx
+
+looplinedefrag:
+cmpl $1024,%esi
+je enddefragmentationfunction
+
+movl $0,%ecx
+loopcolumndefrag:
+cmpl $1024,%ecx
+je changelooplinedefrag
+
+movl $1024,%eax
+movl $0,%edx
+mull %esi
+addl %ecx,%eax
+movl $0,%edx
+
+movb (%edi,%eax,1),%dl
+cmpb $0,%dl
+je changeloopcolumndefrag
+
+movb %dl,id
+
+movl 8(%ebp),%ebx
+movl %ecx,(%ebx) /*punem indicele din stanga */
+movl 16(%ebp),%ebx
+movl %esi,(%ebx) /*punem linia */
+
+defragloopsameid:
+cmpl $1023,%ecx
+je verifdefrag
+
+cmpb id,%dl
+jne defragdecrecx
+
+incl %ecx
+movl $0,%edx
+movl $0,%eax
+mull %esi
+addl %ecx,%eax
+movl $0,%edx
+movb (%edi,%eax,1),%dl
+
+verifdefrag:
+cmpb id,%dl
+je findmyplace
+
+defragdecrecx:
+decl %ecx
+findmyplace:
+movl 12(%ebp),%ebx
+movl %ecx,(%ebx) /*unde se termina id ul */
+
+movl 36(%ebp),%ebx
+movl %ecx,(%ebx) /*salvam ecx ul */
+
+movl 40(%ebp),%ebx
+movl %esi,(%ebx) /* salvam esi ul*/
+
+movl 8(%ebp),%ebx
+movl (%ebx),%ebx
+subl %ebx,%ecx
+incl %ecx /*distanta*/
+
+movl $0,%edx
+movl 52(%ebp),%edx
+movl (%edx),%edx
+movl $1024,%ebx
+subl %edx,%ebx
+
+cmpl %ecx,%ebx
+jae insertonline
+
+nextlineondefrag:
+movl 48(%ebp),%edx
+movl (%edx),%edx
+incl %edx
+movl 48(%ebp),%eax
+movl %edx,(%eax) /*linia = linia + 1 */
+
+movl 52(%ebp),%edx
+movl $0,(%edx) /*coloana este 0*/
+
+insertonline:
+movl 28(%ebp),%esi
+movl 20(%ebp),%eax
+movl %ecx,(%eax) /*retinem dist */
+loopidk:
+cmpl $0,%ecx
+je increasetempcolumn
+
+decl %ecx
+movl $1024,%eax
+movl $0,%edx
+movl 48(%ebp),%ebx
+movl (%ebx),%ebx
+mull %ebx
+movl 52(%ebp),%ebx
+movl (%ebx),%ebx
+addl %ebx,%eax
+addl %ecx,%eax   /*t[linia*1024+coloana+i] = elementinit */ 
+
+movl $0,%edx
+movzbl id,%edx
+movb %dl,(%esi,%eax,1)
+jmp loopidk
+
+increasetempcolumn:
+movl 20(%ebp),%eax
+movl (%eax),%eax
+addl %eax,%ebx
+movl 52(%ebp),%eax
+movl %ebx,(%eax) /*coloana = coloana + dist */
+
+movl 36(%ebp),%ecx
+movl (%ecx),%ecx
+
+movl 40(%ebp),%esi
+movl (%esi),%esi
+
+movl 20(%ebp),%ebx
+movl (%ebx),%ebx
+
+movl 32(%ebp),%edi
+stergemdinmem:
+cmpl $0,%ebx
+je revenireregistrii
+
+movl $0,%edx
+movl $1024,%eax
+mull %esi
+addl %ecx,%eax
+movl $0,%edx
+movb %dl,(%edi,%eax,1)
+decl %ecx
+decl %ebx
+
+revenireregistrii:
+incl %ecx
+movl 36(%ebp),%ebx
+movl %ecx,(%ebx)
+
+movl 40(%ebp),%esi
+movl (%esi),%esi
+
+
+changeloopcolumndefrag:
+incl %ecx
+jmp loopcolumndefrag
+
+changelooplinedefrag:
+incl %esi
+jmp looplinedefrag
+
+enddefragmentationfunction:
+popl %ebp
+ret
+
+
+initializareCuZero:
+pushl %ebp
+movl %esp,%ebp
+
+movl 8(%ebp),%esi
+
+movl $0,%ebx
+movl $0,%ecx
+looplineinitzero:
+cmpl $1024,%ebx
+je endinitializareCuZero
+
+movl $0,%ecx
+loopcolumninitzero:
+cmpl $1024,%ecx
+je changelineinitzero
+
+movl $0,%edx
+movl $1024,%eax
+mull %ebx
+addl %ecx,%eax
+
+movl $0,%edx
+movb %dl,(%esi,%eax,1)
+incl %ecx
+
+changelineinitzero:
+incl %ebx
+jmp looplineinitzero
+
+endinitializareCuZero:
+popl %ebp
+ret
+
+
+changeelements:
+pushl %ebp
+movl %esp,%ebp
+
+movl 8(%ebp),%esi
+movl 12(%ebp),%edi
+
+movl $0,%ebx
+movl $0,%ecx
+linechangeelements:
+cmpl $1024,%ebx
+je endchangeelements
+
+movl $0,%ecx
+columnchangeelements:
+cmpl $1024,%ecx
+je lineincreaseelements
+
+movl $0,%eax
+movl $0,%edx
+mull %ebx
+addl %ecx,%eax
+movl $0,%edx
+
+movb (%esi,%eax,1),%dl
+movb %dl,(%edi,%eax,1)
+
+incl %ecx
+
+lineincreaseelements:
+incl %ebx
+jmp linechangeelements
+
+endchangeelements:
+popl %ebp
+ret
+
+
 .global main
 
 main:
@@ -626,6 +877,77 @@ jmp operationsloop
 
 
 defragmentationoperation:
+lea memorie,%edi
+lea tempmemorie,%esi
+
+pushl %ecx
+pushl %esi
+call initializareCuZero
+popl %eax
+popl %ecx
+
+movl $0,%eax
+movl %eax,coloana
+movl %eax,linia
+
+lea memorie,%edi
+lea tempmemorie,%esi
+
+pushl %ecx
+pushl $coloana
+pushl $linia
+pushl $saveEAX
+pushl $saveEBX
+pushl $saveECX
+pushl %edi
+pushl %esi
+pushl $elementinit
+pushl $distanta
+pushl $indLinie
+pushl $indJDR
+pushl $indJST
+call defragmentationfunction
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %ecx
+
+
+lea memorie,%edi
+lea memorie,%esi
+
+pushl %ecx
+pushl %edi
+pushl %esi
+call changeelements
+popl %eax
+popl %eax
+popl %ecx
+
+lea tempmemorie,%edi
+
+pushl %ecx
+pushl $indLinie
+pushl $indJDR
+pushl $indJST
+pushl %edi
+call printmemoryfunction
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %ecx
+
+
 decl %ecx
 jmp operationsloop
 
