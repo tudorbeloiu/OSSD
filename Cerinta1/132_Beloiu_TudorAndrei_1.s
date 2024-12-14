ec920999c16f78c259c07e6be50691361a2d23b6
tudorbeloiu <beloiutudorandrei@gmail.com>
2024-12-14 18:00:39 +0200

    Implemented concrete, just need to add files to memory

--- Cerinta1/132_Beloiu_TudorAndrei_1.s
+++ Cerinta1/132_Beloiu_TudorAndrei_1.s
@@ -24,6 +24,25 @@ getOutputZero: .asciz "((0, 0), (0, 0))\n"
 outputZero: .asciz "%d: ((0, 0), (0, 0))\n"
 intervalOutput: .asciz "%d: ((%d, %d), (%d, %d))\n"
 otpget: .asciz "((%d, %d), (%d, %d))\n"
+/* mai jos sunt pt concrete */
+max_path_size: .space 256
+dir: .space 256
+str_input: .asciz "%s"
+str_output: .asciz "%s\n"
+int_output: .asciz "%d\n"
+format_path: .asciz "%s/%s"
+dir_ptr: .space 4
+file_name: .space 256
+full_file_path: .space 512
+offset: .long 11 /* pentru d_name, structura dirent are mai multe campuri */
+dot: .asciz "."
+dotdot: .asciz ".."
+fd: .space 4
+fd_nou: .space 4
+file_stat: .space 96
+st_size: .space 128
+msj_complet: .asciz "File: %s, Descriptor: %d, Size: %d\n"
+
 
 .text
 
@@ -750,6 +769,8 @@ je deleteoperation
 cmpl $4,%eax
 je defragmentationoperation
 
+cmpl $5,%eax
+je concreteoperation
 
 addoperation:
 pushl %ecx
@@ -976,6 +997,156 @@ popl %ecx
 
 decl %ecx
 jmp operationsloop
+
+
+concreteoperation:
+pushl %ecx
+pushl $dir
+pushl $str_input
+call scanf
+addl $8,%esp
+popl %ecx
+
+pushl %ecx
+pushl $dir
+call opendir
+addl $4,%esp
+popl %ecx
+
+cmpl $0,%eax
+je print_memory_concrete /* daca eax este 0 (NULL), inseamna ca nu putem deschide directorul */
+
+movl %eax,dir_ptr /* retin in dir_ptr adresa directorului */
+
+et_read_files:
+pushl %ecx
+movl dir_ptr,%eax
+pushl %eax
+call readdir
+addl $4,%esp
+popl %ecx
+
+movl %eax,%ebx
+testl %ebx,%ebx
+jz close_dir
+
+
+addl offset,%ebx
+movl %ebx,file_name /* salvez pointer catre numele fisierului(fara concatenare la path) */
+/* offset ul este necesar ca sa ajung la d_name din structura dirent */
+
+/* acum verificam daca numele este diferit de . si de .. ( director curent si dir parinte ) */
+
+pushl %ecx
+pushl $dotdot
+pushl %ebx
+call strcmp
+addl $8,%esp
+popl %ecx
+
+testl %eax,%eax
+jz et_read_files
+
+pushl %ecx
+pushl $dot
+pushl %ebx
+call strcmp
+addl $8,%esp
+popl %ecx
+
+testl %eax,%eax
+jz et_read_files
+
+/* acum construiesc path ul full /home/tudorbeloiu/.../file1.txt */
+/* int snprintf(char *str, size_t size,const char *format , x1, x2 ) */
+
+pushl %ecx
+pushl %ebx /* contine entry -> d_name */
+pushl $dir
+pushl $format_path
+pushl $512
+pushl $full_file_path
+call snprintf
+addl $20,%esp /*snprintf rescrie complet full_path cu noua cale completa */
+popl %ecx
+
+pushl %ecx
+pushl $0 /* 0_RDNLY */
+pushl $full_file_path
+call open
+addl $8,%esp
+popl %ecx
+
+/* fd este in eax */
+
+movl %eax,fd
+movl %eax,fd_nou
+movl %eax,%ebx
+testl %ebx,%ebx
+
+js et_read_files
+
+/* noi trebuie sa facem (fd % 255) + 1 */
+movl fd_nou,%eax
+movl $0,%edx
+movl $255,%ebx
+divl %ebx
+movl $0,%ebx
+movl %edx,%ebx
+incl %ebx
+movl %ebx,fd_nou
+
+pushl %ecx
+pushl $file_stat
+pushl fd
+call fstat
+addl $8,%esp
+popl %ecx
+
+cmpl $0,%eax /* daca eax este -1 inseamna ca avem o eroare */
+jl et_read_files
+
+movl $file_stat,%eax
+movl 44(%eax),%ebx /* offset ul pana la st_size este 44 */
+
+pushl %ecx
+pushl %ebx
+pushl fd_nou
+pushl $full_file_path
+pushl $msj_complet
+call printf
+addl $16,%esp
+popl %ecx
+
+jmp et_read_files
+
+close_dir:
+pushl %ecx
+pushl %ebx
+call closedir
+addl $4,%esp
+popl %ecx
+
+jmp print_memory_concrete
+
+print_memory_concrete:
+lea memorie,%edi
+
+pushl %ecx
+pushl $indLinie
+pushl $indJDR
+pushl $indJST
+pushl %edi
+call printmemoryfunction
+popl %eax
+popl %eax
+popl %eax
+popl %eax
+popl %ecx
+
+decl %ecx
+jmp operationsloop
+
 
 et_exit:
 pushl $0
